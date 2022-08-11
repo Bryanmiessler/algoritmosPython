9587a1acc9306b958ae803676ae2a01c2575425e
Bryanmiessler <45339280+Bryanmiessler@users.noreply.github.com>
2022-08-11 15:49:37 -0500

    Update algoritmos.py
    Cambiando el orden dentro del if principal

--- algoritmos.py
+++ algoritmos.py
@@ -3,58 +3,59 @@ num1 = int(input('Ingrese el primer numero: '))
 num2 = int(input('Ingrese el segundo numero: '))
 num3 = int(input('Ingrese el tercer numero: '))
 
-if ordenamiento == 'descendente':
+
+if ordenamiento == 'ascendente':
     if num1 > num2 and num2 > num3:
-        print(num1,num2,num3)
+        print(num3,num2,num1)
     elif num1 > num3 and num3 > num2:
-        print(num1,num3,num2)
+        print(num2,num3,num1)
     elif num2 > num1 and num1 > num3:
-        print(num2,num1,num3)
+        print(num3,num1,num2)
     elif num2 > num3 and num3 > num1:
-        print(num2,num3,num1)
-    elif num3 > num2 and num2 > num1:
-        print(num3,num2,num1)
-    elif num3 > num1 and num1 > num2:
-        print(num3,num1,num2)
-    elif num3 == num1 and num1 > num2:
-        print(num3,num1,num2)
-    elif num3 == num2 and num2 > num1:
-        print(num3,num2,num1)
-    elif num2 == num1 and num1 > num3:
-        print(num2,num1,num3)
-    elif num2 == num3 and num3 > num1:
-        print(num2,num3,num1)
-    elif num1 == num2 and num2 > num3:
-        print(num1,num2,num3)
-    elif num1 == num3 and num3 > num2:
         print(num1,num3,num2)
+    elif num3 > num2 and num2 > num1:
+        print(num1,num2,num3)
+    elif num3 > num1 and num1 > num2:
+        print(num2,num1,num3)
+    elif num3 == num1 and num1 > num2:
+        print(num2,num1,num3)
+    elif num3 == num2 and num2 > num1:
+        print(num1,num2,num3)
+    elif num2 == num1 and num1 > num3:
+        print(num3,num1,num2)
+    elif num2 == num3 and num3 > num1:
+        print(num1,num3,num2)
+    elif num1 == num2 and num2 > num3:
+        print(num3,num2,num1)
+    elif num1 == num3 and num3 > num2:
+        print(num2,num3,num1)
     else:
         print('Todos los numeros son iguales')
-elif ordenamiento == 'ascendente':
+elif ordenamiento == 'descendente':
     if num1 > num2 and num2 > num3:
-        print(num3,num2,num1)
+        print(num1,num2,num3)
     elif num1 > num3 and num3 > num2:
-        print(num2,num3,num1)
+        print(num1,num3,num2)
     elif num2 > num1 and num1 > num3:
-        print(num3,num1,num2)
+        print(num2,num1,num3)
     elif num2 > num3 and num3 > num1:
-        print(num1,num3,num2)
-    elif num3 > num2 and num2 > num1:
-        print(num1,num2,num3)
-    elif num3 > num1 and num1 > num2:
-        print(num2,num1,num3)
-    elif num3 == num1 and num1 > num2:
-        print(num2,num1,num3)
-    elif num3 == num2 and num2 > num1:
-        print(num1,num2,num3)
-    elif num2 == num1 and num1 > num3:
-        print(num3,num1,num2)
-    elif num2 == num3 and num3 > num1:
-        print(num1,num3,num2)
-    elif num1 == num2 and num2 > num3:
-        print(num3,num2,num1)
-    elif num1 == num3 and num3 > num2:
         print(num2,num3,num1)
+    elif num3 > num2 and num2 > num1:
+        print(num3,num2,num1)
+    elif num3 > num1 and num1 > num2:
+        print(num3,num1,num2)
+    elif num3 == num1 and num1 > num2:
+        print(num3,num1,num2)
+    elif num3 == num2 and num2 > num1:
+        print(num3,num2,num1)
+    elif num2 == num1 and num1 > num3:
+        print(num2,num1,num3)
+    elif num2 == num3 and num3 > num1:
+        print(num2,num3,num1)
+    elif num1 == num2 and num2 > num3:
+        print(num1,num2,num3)
+    elif num1 == num3 and num3 > num2:
+        print(num1,num3,num2)
     else:
         print('Todos los numeros son iguales')
 else:
